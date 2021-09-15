1240676fd6202c800af0628dc3e4a2c154503a58
NDL2000 <76539013+NDL2000@users.noreply.github.com>
2021-09-15 14:25:03 +0700

    update version 210915
    update version 210915

--- admin/QLDG/duyet_submit.php
+++ admin/QLDG/duyet_submit.php
@@ -4,7 +4,7 @@ $id = $_GET['id'];
 $sql = "update danhgia set TrangThai=1 WHERE MaDG = '$id' " ; 
 $result= mysqli_query($conn,$sql);
 if (mysqli_query($conn, $sql)) {
-    header("Location:./index.php?url=qldg?kq=1");
+    header("Location:./index.php?url=qldg&kq=1");
 } 
 mysqli_close($conn);
 ?>
