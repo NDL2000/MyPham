4fa2cb64f5b8204162163851a7ed12b55c7a0376
NDL2000 <nguyenducly2000@gmail.com>
2021-10-15 09:27:14 +0700

    update version 151021

--- admin/QLDG/xoa.php
+++ admin/QLDG/xoa.php
@@ -4,7 +4,7 @@ $id = $_GET['id'];
 $sql = "DELETE FROM danhgia WHERE MaDG = '$id'";
 $result= mysqli_query($conn,$sql);
 if (mysqli_query($conn, $sql)) {
-     header("Location:./index.php?url=qldg&kq=2");
+     echo "<script>window.location.href='./index.php?url=qldg&kq=2'</script>";
 }
 mysqli_close($conn);
 ?>
